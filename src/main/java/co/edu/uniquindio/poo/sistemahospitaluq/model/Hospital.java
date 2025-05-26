811c31b922a12acb6dda9cff71575f160d7ea062
Juan Gordillo <juanc.gordilloh@uqvirtual.edu.co>
2025-05-25 19:29:47 -0500

    Agregar clase abstracta Usuario y Hospital

--- src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Hospital.java
+++ src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Hospital.java
@@ -1,4 +1,17 @@
 package co.edu.uniquindio.poo.sistemahospitaluq.model;
 
 public class Hospital {
+
+    private String nombre;
+    public Hospital(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Usuario.java
+++ src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Usuario.java
@@ -1,4 +1,50 @@
 package co.edu.uniquindio.poo.sistemahospitaluq.model;
 
-public class Usuario {
+public abstract  class Usuario {
+
+    protected String nombre;
+    protected String cedula;
+    protected String correo;
+    protected String telefono;
+
+    public Usuario(String nombre, String cedula, String correo, String telefono){
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    // getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
