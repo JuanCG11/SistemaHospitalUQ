344044ee5828e29f659418767f5b8c0b4751b13c
Juan Gordillo <juanc.gordilloh@uqvirtual.edu.co>
2025-05-25 22:19:21 -0500

    Agregar clase Administrador

--- src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Administrador.java
+++ src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Administrador.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.poo.sistemahospitaluq.model;
 
-public class Administrador {
+public class Administrador extends Usuario{
+
+    public Administrador(String id, String nombre, String correo, String telefono) {
+        super(id, nombre, correo, telefono);
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Administrador";
+    }
 }
