9f4e3595d3e186ce949e319f897bdef950106b1d
Juan Gordillo <juanc.gordilloh@uqvirtual.edu.co>
2025-05-25 22:23:54 -0500

    Agregar clase Hospital

--- src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Hospital.java
+++ src/main/java/co/edu/uniquindio/poo/sistemahospitaluq/model/Hospital.java
@@ -1,10 +1,31 @@
 package co.edu.uniquindio.poo.sistemahospitaluq.model;
 
+import java.util.ArrayList;
+
 public class Hospital {
 
     private String nombre;
-    public Hospital(String nombre){
+    private ArrayList<Paciente> pacientes;
+    private ArrayList<Medico> medicos;
+    private ArrayList<Administrador> administradores;
+
+    public Hospital(String nombre) {
         this.nombre = nombre;
+        this.pacientes = new ArrayList<>();
+        this.medicos = new ArrayList<>();
+        this.administradores = new ArrayList<>();
+    }
+
+    public void registrarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
+
+    public void registrarMedico(Medico medico) {
+        medicos.add(medico);
+    }
+
+    public void registrarAdministrador(Administrador admin) {
+        administradores.add(admin);
     }
 
     public String getNombre() {
@@ -13,5 +34,29 @@ public class Hospital {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public ArrayList<Paciente> getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(ArrayList<Paciente> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    public ArrayList<Medico> getMedicos() {
+        return medicos;
+    }
+
+    public void setMedicos(ArrayList<Medico> medicos) {
+        this.medicos = medicos;
+    }
+
+    public ArrayList<Administrador> getAdministradores() {
+        return administradores;
+    }
+
+    public void setAdministradores(ArrayList<Administrador> administradores) {
+        this.administradores = administradores;
     }
 }
